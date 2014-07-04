a90b5f1ce5a6d266fde35e04882ae246a156817a
nhz2f <1817556010@qq.com>
2014-07-03 23:59:52 -0700

    make a basic User model (inclouding secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
 
   #get 'static_pages/home'
-  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
